5dcf528d7691e9ea854e1a52bf05f9597b1e0af6
Josue Abotsidia <josueabots@gmail.com>
2022-09-09 06:15:14 -0700

    fixing error

--- env1.c
+++ env1.c
@@ -63,7 +63,7 @@ int i, j;
 for (i = 0; datash->_environ[i]; i++)
 {
 for (j = 0; datash->_environ[i][j]; j++)
-{
+;
 write(STDOUT_FILENO, datash->_environ[i], j);
 write(STDOUT_FILENO, "\n", 1);
 }
